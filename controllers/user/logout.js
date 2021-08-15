f0fa460b3c132b847a631f98405aad58908af7cb
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2021-08-15 21:58:35 +0530

    logout cors update

--- controllers/user/logout.js
+++ controllers/user/logout.js
@@ -1,5 +1,10 @@
-const logout = (_, res) => {
-  res.clearCookie('loginToken');
+const logout = (req, res) => {
+  res.clearCookie('loginToken', {
+    httpOnly: true,
+    sameSite: req.devEnv ? true : 'none',
+    secure: !req.devEnv,
+  });
+
   res.mk(1);
 };
 
